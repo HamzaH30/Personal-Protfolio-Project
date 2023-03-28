2a4c0e3a484c7f9015371c0d3a84f4b9f2559a40
Hamza Haque <hamzahaque30@gmail.com>
2023-03-28 13:07:56 -0500

    Only show the shadow overlay when user opens menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,11 @@
 function menuClick() {
   menuBtn.classList.toggle("opened-menu");
   mobileMenu.classList.toggle("show-menu");
+  shadowOverlay.classList.toggle("overlay");
 }
 
 const menuBtn = document.getElementById("menu-btn");
 const mobileMenu = document.getElementById("mobile-menu");
+const shadowOverlay = document.getElementById("overlay");
+
 menuBtn.addEventListener("click", menuClick);
