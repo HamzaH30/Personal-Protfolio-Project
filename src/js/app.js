eda83f4feadc31cbc90fa84c0f3fa82699ae29c6
Hamza Haque <hamzahaque30@gmail.com>
2023-03-28 12:54:04 -0500

    Add JS to show/hide mobile menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 function menuClick() {
   menuBtn.classList.toggle("opened-menu");
+  mobileMenu.classList.toggle("show-menu");
 }
 
 const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
 menuBtn.addEventListener("click", menuClick);
