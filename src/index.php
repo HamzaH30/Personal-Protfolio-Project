e0c30739ed6dbddab7205deb2eb682f74224848c
Hamza Haque <hamzahaque30@gmail.com>
2024-06-06 05:39:35 -0500

    Restarting

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home - Hamza Haque</title>
+</head>
+
+<body>
+  <footer>
+    <p>Hamza Haque &copy; <?= date('Y') ?></p>
+  </footer>
+</body>
+
+</html>
