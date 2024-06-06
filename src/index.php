89b20733a12c6b84e49334dd5367b4f9ab28996e
Hamza Haque <hamzahaque30@gmail.com>
2024-06-06 07:06:45 -0500

    hero page markup done

--- src/index.php
+++ src/index.php
@@ -5,9 +5,45 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Home - Hamza Haque</title>
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200..800&family=Hind+Madurai:wght@300;400;500;600;700&family=Pridi:wght@200;300;400;500;600;700&family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
+
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="styles/style.css">
+
+  <!-- Font Awesome -->
+  <script src="https://kit.fontawesome.com/02a2a00325.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
+  <main>
+    <section class="hero">
+      <section class="img-container">
+        <img src="images/Hamza Haque Headshot.jpg" alt="Hamza Haque Headshot" />
+      </section>
+      <section class="introduction">
+        <section class="intro-text">
+          <h1>Hello, I'm Hamza</h1>
+          <p>A Student, Programmer, And Life-Long Learner</p>
+        </section>
+        <!-- <p>A dual-enrollment graduating student studying Software Development at Manitoba Institute of Trades & Technology (MITT) through the Pembina Trails Early College (PTEC) program. While also a grade 12 student attending Fort Richmond Collegiate (FRC).</p> -->
+        <section class="social-links">
+          <i class="fa-brands fa-linkedin"></i>
+          <i class="fa-brands fa-github"></i>
+          <i class="fa-regular fa-envelope"></i>
+        </section>
+      </section>
+    </section>
+
+    <section>
+
+    </section>
+
+  </main>
+
   <footer>
     <p>Hamza Haque &copy; <?= date('Y') ?></p>
   </footer>
